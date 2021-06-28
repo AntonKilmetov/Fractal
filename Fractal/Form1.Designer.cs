61ccd5a7961aa22ed643404294186ac79b88bfef
AntonKilmetov <86476500+AntonKilmetov@users.noreply.github.com>
2021-06-28 10:28:54 +0500

    Fractal
    Modeling the growth of a fractal on a square lattice in accordance with the Eden model.

---
+++ Fractal/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(707, 707);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(741, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(741, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(738, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество итераций";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(741, 192);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "20";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(738, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Радиус";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(741, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 44);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Увеличить радиус";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(741, 299);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(61, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "20";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(855, 299);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(61, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "20";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(738, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Радиус";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(852, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Масса";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(741, 630);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 44);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "В файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(726, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Вероятность заполнения";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(729, 390);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(61, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "20";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(796, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 707);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Модель Эдена";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Fractal/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        private static int sideLength; // длина стороны
+        private static int R; // радиус
+        private static double p = 0.8; 
+        private int[,] field; // поле занятых ячеек
+        private Point[,] coordinates; // координаты ячеек
+        private List<int> radiusList = new List<int>(); // список радиусов
+        private List<int> massList = new List<int>(); // список масс
+        static SolidBrush black = new SolidBrush(Color.Black);
+        static SolidBrush white = new SolidBrush(Color.White);
+        Random rnd = new Random();
+        Bitmap bmp;
+        Graphics graph;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox5.Text = (p * 100).ToString();
+            R = 1;
+            sideLength = 101;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graph = Graphics.FromImage(bmp);
+            field = new int[sideLength, sideLength];
+            coordinates = new Point[sideLength, sideLength];
+            field[sideLength / 2, sideLength / 2] = 1;
+            radiusList.Add(R);
+            MakeCoord();
+            MassCalculate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Draw();
+        }
+        /// <summary>
+        /// Координаты ячеек
+        /// </summary>
+        private void MakeCoord()
+        {
+            var a = sideLength / 2;
+            for (int i = -sideLength / 2; i < sideLength / 2 + 1; i++)
+                for (int j = -sideLength / 2; j < sideLength / 2 + 1; j++)
+                    coordinates[sideLength / 2 + i, sideLength / 2 + j] = new Point(i, j);
+        }
+        /// <summary>
+        /// Отрисрвка кластера
+        /// </summary>
+        private void Draw()
+        {
+            var length = pictureBox1.Width / sideLength;
+            for (int i = 0; i < sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {                   
+                    if (field[i, j] == 1)
+                        graph.FillRectangle(black, j * length, i * length, length, length);
+                    else
+                        graph.FillRectangle(white, j * length, i * length, length, length);
+                }
+            }
+            pictureBox1.Image = bmp;         
+        }
+        /// <summary>
+        /// Занять ячейку периметра 
+        /// </summary>
+        private void Method()
+        {
+            var a = new double[sideLength, sideLength];
+            for (int i = 0; i < sideLength; i++)
+                for (int j = 0; j < sideLength; j++)
+                    a[i, j] = -1.0;
+            for (int i = 1; i < sideLength - 1; i++)
+            {
+                for (int j = 1; j < sideLength - 1; j++)
+                {
+                    if (field[i, j] == 1 || field[i, j] == -1)
+                    {
+                        if (field[i + 1, j] == 0)
+                            a[i + 1, j] = rnd.NextDouble();
+                        if (field[i - 1, j] == 0)
+                            a[i - 1, j] = rnd.NextDouble();
+                        if (field[i, j + 1] == 0)
+                            a[i, j + 1] = rnd.NextDouble();
+                        if (field[i, j - 1] == 0)
+                            a[i, j - 1] = rnd.NextDouble();
+                    }
+                }
+            }
+            var indexes = GetIndexOfMin(a);
+            if(rnd.NextDouble() < p)
+                field[indexes.Item1, indexes.Item2] = 1;
+            else
+                field[indexes.Item1, indexes.Item2] = -1;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns>минимальный элемент матрицы</returns>
+        private double Min(double[,] matrix)
+        {
+            var minValue = double.MaxValue;
+            foreach (var item in matrix)
+                if (item >= 0 && item < minValue)
+                    minValue = item;
+            return minValue;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns>Индекс минимального элемента</returns>
+        private Tuple<int,int> GetIndexOfMin(double[,] matrix)
+        {
+            var min = Min(matrix);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (Math.Abs(matrix[i, j] - min) < 1E-10)
+                        return Tuple.Create(i, j);
+            return null;
+        }
+        /// <summary>
+        /// Занять ячейку периметра и отрисовать новый кластер
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var n = int.Parse(textBox1.Text);
+            for (int i = 0; i < n; i++)
+            {
+                Method();
+                Draw();
+            }
+        }
+        /// <summary>
+        /// Увеличить радиус
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            R += 1;
+            textBox2.Text = textBox3.Text = R.ToString();
+            radiusList.Add(R);
+            MassCalculate();
+        }
+        /// <summary>
+        /// Рассчитать массу в радиусе R от начальной ячейки
+        /// </summary>
+        private void MassCalculate()
+        {
+            var mass = 0;
+            for (int i = 0; i < sideLength; i++)
+                for (int j = 0; j < sideLength; j++)
+                    if ((coordinates[i, j].X * coordinates[i, j].X + coordinates[i, j].Y * coordinates[i, j].Y) <= (R * R)
+                        && field[i, j] == 1)
+                        mass++;
+            massList.Add(mass);
+            textBox4.Text = mass.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var radius = radiusList
+                .Select(x => Math.Log(x).ToString())
+                .ToArray();
+            var mass = massList
+                .Select(x => Math.Log(x).ToString())
+                .ToArray();
+            File.WriteAllLines(@"C:\anton1\СomputationalPhysics\data.txt", radius);
+
+            File.WriteAllLines(@"C:\anton1\СomputationalPhysics\data2.txt", mass);
+
+            var a = File.ReadAllLines(@"C:\anton1\СomputationalPhysics\data.txt")
+                .Select(x => x.Replace(",", "."))
+                .ToArray();
+            File.WriteAllLines(@"C:\anton1\СomputationalPhysics\data.txt", a);
+
+            var b = File.ReadAllLines(@"C:\anton1\СomputationalPhysics\data2.txt")
+                .Select(x => x.Replace(",", "."))
+                .ToArray();
+            File.WriteAllLines(@"C:\anton1\СomputationalPhysics\data2.txt", b);
+        }
+    }
+}
